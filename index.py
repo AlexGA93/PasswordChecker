d2e7e3b2fd30fee42c0cac633fcab365fab1beca
AlexGA93 <32788331+AlexGA93@users.noreply.github.com>
2020-02-29 09:48:22 +0100

    Index Additions
    First project's implementation of Addition;s function

---
+++ index.py
@@ -0,0 +1,69 @@
+#lists to check in Ascii map
+Upper = list(range(65,91))
+Lower = list(range(97,123))
+Numbers = list(range(48,58))
+Symbols = list(range(33,48))+list(range(58+65))+list(range(91,95))
+
+Middle = Number + Symbols
+Middle.append(96)
+
+#counters
+countersArray = [
+    ['countSize','countUpper','countLower','countNumber','countSymbols','countMiddle','countRequirements'],
+    ['countLettersOnly','countNumbersOnly','countRepChars','countConsUpp','countConsLow','countConsNum','countSeqLetters','countSeqNumbers','countSeqSymbols']
+]
+#Dictionaries(sorting countersArray's elements as dictionaries to better interaction)
+counter_Additions = dict.fromkeys(countersArray[0],0)
+counter_Deductions = dict.fromkeys(countersArray[1],0)
+###############################################################################################################
+                                        #Functions
+def Deductions(password):
+
+def Additions(password):
+    #checking number of characters
+    numberChar = len(password)+1
+    counter_Additions['countSize'] = numberChar
+
+    for element in password:
+        asciiElement = ord(element) #every character will be compared to the dictionaries
+
+        if ascii_element in Upper:
+            counter_Additions['countUpper']+=1
+        elif ascii_element in Lower:
+            counter_Additions['countLower']+=1
+        elif ascii_element in Number:
+            counter_Additions['countNumber']+=1
+        elif ascii_element in Symbols:
+            counter_Additions['countSymbols']+=1
+
+        if (password.index(element) !=0 and password.index(element)!= len(password)-1):
+            if ascii_element in Middle:
+                counter_Additions['countMiddle']+=1
+
+    #Checking dictionaries for requirments
+    for key in counter_Additions:
+        if (counter_Additions[key] > 0 )and (key!= 'countMiddle') and(key!= 'countRequirements'):
+            counter_Additions['countRequirements']+=1
+        
+    result = (counter_Additions['countSize']*4)+((counter_Additions['countSize']-counter_Additions['countUpper'])*2)+((counter_Additions['countSize']-counter_Additions['countLower'])*2)+(counter_Additions['countNumber']*4)+(counter_Additions['countSymbols']*6)+(counter_Additions['countMiddle']*2)+(counter_Additions['countRequirements']*2)
+    
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(counter_additions)
+
+    
+    return(result)
+
+
+
+###############################################################################################################
+def main(counter_Additions, counter_Deductions):
+    password = "Patata93@"
+
+    add_data = Additions(password) #calculating additions to socre
+    deduction_data = Deductions(password) #calculating deductions to score
+
+    
+    score = add_data - deduction_data
+
+if __name__ == "__main__":
+    main(counter_Additions, counter_Deductions)
