e22825c2f3a711d6be9fa7f907083bec39b278ae
AlexGA93 <32788331+AlexGA93@users.noreply.github.com>
2020-04-18 13:45:23 +0200

    Update Deduction
    Update Deduction's method

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ Symbols = list(range(33,48))+list(range(58+65))+list(range(91,97))
 
 Middle = list(range(33,48))+list(range(48,58))+list(range(58,65))+list(range(91,97))+list(range(123,255))
 
-print('Middle: ', Middle)
+#print('Middle: ', Middle)
 #counters
 countersArray = [
     ['countSize','countUpper','countLower','countNumber','countSymbols','countMiddle','countRequirements'],
@@ -20,17 +20,38 @@ counter_Deductions = dict.fromkeys(countersArray[1],0)
 ###############################################################################################################
                                         #Functions
 def Deductions(password):
-    #Checking deductions to actual score
-    #checking duplicated elements
-    for char in password :
-        counts=password.count(char)
-        print(char,counts)
+    #Letters only
+    if(counter_Additions['countNumber']==0 and counter_Additions['countSymbols']==0 ):
+        counter_Deductions['countLettersOnly'] = len(password)
+    #Numbers Only
+    elif(counter_Additions['countUpper']==0 and counter_Additions['countLower']==0 and counter_Additions['countSymbols']==0  ):
+        counter_Deductions['countNumbersOnly'] = len(password)
+    #Repeat Characters
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    rep_char = 0
+    for elem in alphabet:
+        counter = password.count(elem)
+        if(counter>1): rep_char+=counter
+    #print(rep_char)  #5
+    #Consecutive Uppercase
+    #Consecutive Lowercase
+    counter_l=0
+    counter_u=0
+    for i in range(len(password)):
+        checker_u = password[i-1].isupper() == password[i].isupper()
+        checker_l = password[i-1].islower() == password[i].islower()
+        if( (i>0) and (checker_u == password[i].isupper()) ):
+            counter_u+=1
+        elif( (i>0) and (checker_l == password[i].islower()) ):
+            counter_l+=1
+    print('Contador Mayusculas: ',counter_u-1,'\nContador Minusculas: ',counter_l-1)
+    #Consecutive Numbers
 
-    
+    #Sequential Numbers
+    #Sequentials Symbols
 
-    result = 'hola esto es temporal'
-    return result
-
+    #return result
+    return 'temporal'
 def Additions(password):
     #checking number of characters
     numberChar = len(password)
@@ -58,10 +79,10 @@ def Additions(password):
             counter_Additions['countRequirements']+=1
         
     result = (counter_Additions['countSize']*4)+((counter_Additions['countSize']-counter_Additions['countUpper'])*2)+((counter_Additions['countSize']-counter_Additions['countLower'])*2)+(counter_Additions['countNumber']*4)+(counter_Additions['countSymbols']*6)+(counter_Additions['countMiddle']*2)+(counter_Additions['countRequirements']*2)
-    
+    '''
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(counter_Additions)
-
+    '''
     
     return(result)
 
@@ -69,7 +90,7 @@ def Additions(password):
 
 ###############################################################################################################
 def main(counter_Additions, counter_Deductions):
-    password = "Patata93@"
+    password = "PAatata93@"
 
     add_data = Additions(password) #calculating additions to socre
     '''
@@ -79,7 +100,7 @@ def main(counter_Additions, counter_Deductions):
     '''
     deduction_data = Deductions(password) #calculating deductions to score
 
-    print(deduction_data)
+    #print('Deductions: ',deduction_data)
     #score = add_data - deduction_data
 
 if __name__ == "__main__":
